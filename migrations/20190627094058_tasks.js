f9d62ce69d47a08ca62d0f2849692c66164b7a97
harrisoares <kelvenlima2009@hotmail.com>
2019-06-27 10:27:59 -0300

    Criacao tabela att

--- migrations/20190627094058_tasks.js
+++ migrations/20190627094058_tasks.js
@@ -1,7 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("tasks", (table) => {
-        table.increments('old').primary();
+        table.increments('oid').primary();
         table.string('title').notNullable();
         table.string('description').notNullable();
         table.timestamps();
@@ -9,5 +9,5 @@ exports.up = function(knex, Promise) {
   };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable("taks");
+  return knex.schema.dropTable("tasks");
   };
